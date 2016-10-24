c1ef9509283d7b2d2f7fa41f609d68cd174ca34f
jasonwu <jason.wu.en@icloud.com>
2016-10-24 13:11:26 +0800

    formatter code

--- src/main/java/cn/net/wujun/Application.java
+++ src/main/java/cn/net/wujun/Application.java
@@ -1,13 +1,7 @@
 package cn.net.wujun;
 
-import cn.net.wujun.common.annotation.ComponentScanExclude;
 import cn.net.wujun.common.support.CustomSpringApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -20,6 +14,5 @@ public class Application {
 
     public static void main(String[] args) {
         CustomSpringApplication.run(Application.class, args);
-        //SpringApplication.run(Application.class, args);
     }
 }
